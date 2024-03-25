17ce7f34f7a64439157e901219c4174798a7e08f
Sanerix <eric.faddeev@gmail.com>
2024-03-25 09:43:48 +0100

    Fix file path in index.html controller

--- web/index.html.controller.ts
+++ web/index.html.controller.ts
@@ -8,7 +8,7 @@ export class IndexHtmlController {
     @Get()
     async getIndex(req : Request, res: Response) {
         // read index.html file from disk
-        const indexHtml = await fs.promises.readFile('./src/example/web/index.html', 'utf8')
+        const indexHtml = await fs.promises.readFile('./web/index.html', 'utf8')
         return indexHtml
     }
 }
